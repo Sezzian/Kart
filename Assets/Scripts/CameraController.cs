ea97495e2752c208ada3a8287953c68748a6a2d8
Sezzian <reidys@student.ethz.ch>
2020-10-21 22:00:08 +0200

    control&camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject kart;
+
+    private Vector3 SpaceOffset;
+    private Vector3 RotationOffset;
+    private Quaternion rotation;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpaceOffset = transform.position - kart.transform.position;
+        RotationOffset = transform.eulerAngles - kart.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation = Quaternion.Euler(0.0f,kart.transform.eulerAngles.y, 0.0f);
+        transform.position = kart.transform.position + rotation * SpaceOffset;
+        transform.eulerAngles = kart.transform.eulerAngles + RotationOffset;
+    }
+}
---
+++ Assets/Scripts/Keyboard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour
+{
+    public int PlayerSpeed = 1;
+    private float SpeedOffset = 0.1f;
+
+    private float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Speed = PlayerSpeed * SpeedOffset;
+
+        Vector3 velocity = new Vector3(0.0f, 0.0f, Speed*moveVertical);
+        Vector3 rotation = new Vector3(0.0f, moveHorizontal, 0.0f);
+
+        if (moveVertical == 0f) {
+            rotation = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        transform.Translate(velocity);
+        transform.Rotate(rotation);
+    }
+}
