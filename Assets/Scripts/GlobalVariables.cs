1e4b953216028f7d16f26ec142923785fc9c7f97
Sezzian <reidys@student.ethz.ch>
2020-11-18 16:06:24 +0100

    User Interface

---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+    // police car == false, taxi == true
+    public bool player;
+    public float speed;
+    public float sound;
+
+    public static GlobalVariables Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public Slider SpeedSlider;
+    public Slider SoundSlider;
+
+    public void GotoMainPolice()
+    {
+        GlobalVariables.Instance.player = false;
+        GlobalVariables.Instance.speed = SpeedSlider.value;
+        GlobalVariables.Instance.sound = SoundSlider.value;
+
+        SceneManager.LoadScene("Main");
+    }
+
+    public void GotoMainTaxi()
+    {
+        GlobalVariables.Instance.player = true;
+        GlobalVariables.Instance.speed = SpeedSlider.value;
+        GlobalVariables.Instance.sound = SoundSlider.value;
+
+        SceneManager.LoadScene("Main");
+    }
+}
